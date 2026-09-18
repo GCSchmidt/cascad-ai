fb3472b9eb4170425fa08b98dd361691b1ab45e4
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-09-18 13:23:40 +0200

    Token Class: added confidence property

--- src/cascadai/schema/token_piece.py
+++ src/cascadai/schema/token_piece.py
@@ -11,11 +11,12 @@ class Token_Type(Enum):
 
 class Token():
 
-    def __init__(self, type: Token_Type, x: int, y: int, width: int) -> None:
+    def __init__(self, type: Token_Type, x: int, y: int, width: int, confidence: float = 0) -> None:
         self.type = type
         self.x = x
         self.y = y
         self.width = width
+        self.confidence = confidence
 
     def __repr__(self) -> str:
-        return f"type: {self.type.name} | x: {self.x} | y: {self.y} | width: {self.width} "
+        return f"type: {self.type.name} | x: {self.x} | y: {self.y} | width: {self.width} | confidence: {self.confidence}"
